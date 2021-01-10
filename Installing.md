6e88eb0a7d759ee305c1c661184f11f1b6b9b69a
Farhan Hai Khan <njrfarhandasilva10@gmail.com>
2021-01-10 17:55:29 +0530

    Homepage for Flask
    Favicon Missing

--- Installing.md
+++ Installing.md
@@ -3,3 +3,7 @@ MedicAIEnv\Scripts\activate
 pip install -r reqs.txt
 python Gen.py
 python DocxGen.py
+
+MedicAIEnv\Scripts\activate
+To deactivate base - conda.bat deactivate
+cd WebsiteFiles/FlaskApp
---
+++ WebsiteFiles/FlaskApp/FlaskApplication.py
@@ -0,0 +1,67 @@
+"""
+MedicAI is an initiative by Team MLXTREME for the Hackathon.
+Medical Artificial Intelligence.
+Assistive Technology for better Electronic Healthcare Facilities for a brighter tomorrow.
+
+To get a demo, Run app.py and visit the link which comes up. 
+
+Regards, 
+Team MLXTREME
+
+Instructions For Running :
+
+MedicAIEnv\Scripts\activate
+To deactivate base - conda.bat deactivate
+cd WebsiteFiles/FlaskApp
+python FlaskApplication.py
+
+
+TODO :
+Favicon
+"""
+
+# import Flask Library
+
+from flask import Flask, render_template, request, url_for, send_from_directory
+import os
+
+app = Flask(__name__, static_folder="assets")
+
+
+@app.route('/')
+def home1():
+    return render_template('homepage.html')
+
+
+@app.route('/index.html')
+def home2():
+    return render_template('homepage.html')
+
+
+@app.route('/index')
+def home3():
+    return render_template('homepage.html')
+
+
+@app.route('/home')
+def home4():
+    return render_template('homepage.html')
+
+
+@app.route('/home.html')
+def home():
+    return render_template('homepage.html')
+
+
+@app.route('/homepage.html')
+def homepage():
+    return render_template('homepage.html')
+
+
+@app.route('/homepage.html')
+def homepage1():
+    return render_template('homepage.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
